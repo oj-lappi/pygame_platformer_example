73095ba0e02445aa1e17f3d3db90e586392129c3
oj_lappi <olappi@abo.fi>
2019-01-16 13:26:20 +0200

    Add files via upload

--- game_object.py
+++ game_object.py
@@ -8,16 +8,22 @@ class GameObject:
 		self.acceleration_y = acceleration_y
 		self.image = image
 		self.time_to_live = time_to_live
+		
+		self.collisionBox = image.get_rect()
 	
 	def update(self,screen):
 		self.x += self.hastighet_x
 		self.y += self.hastighet_y
+		
+		self.collisionBox.x = self.x
+		self.collisionBox.y = self.y
+		
 		window_height = screen.get_height()
 		window_width = screen.get_width()
 		
 		if self.time_to_live != 0:
-			screen.blit( self.image, (self.x%window_width,self.y%window_height) )
-			screen.blit( self.image, (self.x%window_width-window_width,self.y%window_height-window_height) )
+			screen.blit( self.image, (self.x,self.y) )
+			#screen.blit( self.image, (self.x%window_width-window_width,self.y%window_height-window_height) )
 			self.time_to_live -= 1
 		else:
 			del self
